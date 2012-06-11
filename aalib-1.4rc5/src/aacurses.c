05c6ab869e6a46b6ed54e9a4d57d5ebd4b0924de
Bill Agee <billagee@gmail.com>
2012-06-10 22:22:27 -0700

    Fix compile failure on OSX 10.7 - use aacurses.c patch from netbsd pkg collection: http://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/graphics/aalib/patches/

--- aalib-1.4rc5/src/aacurses.c
+++ aalib-1.4rc5/src/aacurses.c
@@ -71,8 +71,8 @@ static void curses_getsize(aa_context * c, int *width, int *height)
 {
     if (__resized_curses)
 	curses_uninit(c), curses_init(&c->params, NULL,&c->driverparams, NULL), __resized_curses = 0;
-    *width = stdscr->_maxx + 1;
-    *height = stdscr->_maxy + 1;
+    *width = getmaxx(stdscr) + 1;
+    *height = getmaxy(stdscr) + 1;
 #ifdef GPM_MOUSEDRIVER
     gpm_mx = *width;
     gpm_my = *height;
